a52e4e0d17e1c8d25d40da801332da9bf70421e7
Z178237 <56522737+Z178237@users.noreply.github.com>
2025-03-31 15:59:23 +1030

    update about the bonus
    Bonus 1: Completion of expires freshness checking; support for the combination of expires and max-age to determine the validity of the cache, compliant with RFC2616.

--- Proxy-bonus.py
+++ Proxy-bonus.py
@@ -8,11 +8,54 @@ import time
 import email.utils
 import datetime
 
+cacheData =0 
 fresh = False
 max_age = 0
 expires_time = 0
 
-with open('proxy.conf', 'r') as cacheFile:
-    cacheFile.write(response_data)
+for line in cacheData:
+    if "Cache-control" in line:
+        if "no-cache" in line or "no-store" in line:
+            fresh = False
+        elif "max-age" in line:
+            max_age = int(line.split("=")[1])
+            
+    if "Expires" in line:
+        expires_str = line.split(":", 1)[1].strip()
+        try:
+            expires_time = email.utils.mktime_tz(email.utils.parsedate_tz(expires_str))
+        except TypeError:
+            expires_time = 0
+            
+if expires_time:
+    if expires_time < time.time():
+        fresh = False
+    else:
+        fresh = True
+elif max_age is not None and max_age>0:
+    if time.time() - last_modified_time <= max_age:
+        fresh = True
+    else:
+        fresh = False
 
+if not fresh:
+    cacheFile.close()
+    raise Exception("Cache expired")
+
+response_data = "".join(cacheData)
+clientSocket.sendall(response_data.encode('utf-8'))
+print(f"response sent to client: {response_data}")
+
+with open(cacheLocation, 'wb') as cacheFile:
+    cacheFile.write(response_data.encode('utf-8'))
     
+    
+        
+        
+        
+        
+
+        
+
+
+
