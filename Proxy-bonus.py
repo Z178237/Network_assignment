133ab32b7b9402338339db3d0242c32b6f68d20a
Z178237 <56522737+Z178237@users.noreply.github.com>
2025-03-31 15:29:48 +1030

    try to solve the question about the bonus

---
+++ Proxy-bonus.py
@@ -0,0 +1,18 @@
+# Include the libraries for socket and system calls
+import socket
+import sys
+import os
+import argparse
+import re
+import time
+import email.utils
+import datetime
+
+fresh = False
+max_age = 0
+expires_time = 0
+
+with open('proxy.conf', 'r') as cacheFile:
+    cacheFile.write(response_data)
+
+    
